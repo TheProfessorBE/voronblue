b9ce4ece3b30b6f2f0b7295915f5891e0ecb2b7a
Jan <jan.steckel@gmail.com>
2022-12-13 21:25:02 +0000

    Autocommit from 2022-12-13 21:25:02
    Klipper on commit: 5b1a667
    
    Moonraker on commit: 5a3b1b6
    
    Mainsail version: v2.3.1

--- toggleOverheadLight.py
+++ toggleOverheadLight.py
@@ -2,23 +2,19 @@ import requests
 import json
 import os
 
-FILENAME = '/home/pi/klipper_config/lightToggleFile.dat'
+#FILENAME = '/home/pi/klipper_config/lightToggleFile.dat'
 
-#FILENAME = "storage"
-if( os.path.exists( FILENAME ) ):
-    print( "on" )
-    url = 'http://energysocket-2933CE.local/api/v1/state'
-    input_dict = {'power_on': True, 'switch_lock': False, 'brightness': 1}
-    headers = {"Content-Type": "application/json"}
-    response = requests.put(url, data=json.dumps(input_dict), headers=headers)
-    
-    os.remove(FILENAME)
-    
-else:
-    print("off")
-    url = 'http://energysocket-2933CE.local/api/v1/state'
+FILENAME = "storage"
+url = 'http://energysocket-2933CE.local/api/v1/state'
+headers = {"Content-Type": "application/json"}
+
+response = requests.get(url )
+response_json = response.json()
+json_data = json.loads(response.text)
+
+if(json_data['power_on']):
     input_dict = {'power_on': False, 'switch_lock': False, 'brightness': 1}
-    headers = {"Content-Type": "application/json"}
-    response = requests.put(url, data=json.dumps(input_dict), headers=headers)
+else:
+    input_dict = {'power_on': True, 'switch_lock': False, 'brightness': 1}
 
-    f = open(FILENAME, "w")
+response = requests.put(url, data=json.dumps(input_dict), headers=headers)
